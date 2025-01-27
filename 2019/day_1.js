b75b0639d7026e8f2d93e96517eae79350cf67a1
Inkeet C <inkeetcha234@gmail.com>
2025-01-27 22:44:02 +0530

    day 1 part 2 done

--- 2019/day_1.js
+++ 2019/day_1.js
@@ -1,9 +1,12 @@
 const data = Deno.readTextFileSync("day_1_data.txt");
-
+// const data = "100756";
 let sum = 0;
 
 data.split("\n").forEach((value) => {
-  sum += Math.floor(Number(value) / 3) - 2;
+  while (value > 0) {
+    value = Math.floor(Number(value) / 3) - 2;
+    sum += value > 0 ? value : 0;
+  }
 });
 
 console.log(sum);
